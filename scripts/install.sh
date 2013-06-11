a3b5698907f243d0c2cb72d5c9adda69926072c3
Fyodor Y <fygrave@o0o.nu>
2013-06-11 12:59:07 +0800

    some extra install scripts

---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+#!/bun.bash
+
+apt get install python-pip
+
+apt-get install flow-tools
+apt-get install fprobe
+apt-get install bridge-utils
+
+
+# brctl addbr br0
+# ip addr show
+# brctl addif br0 eth0 eth1
+
+
+pip install virtenv
