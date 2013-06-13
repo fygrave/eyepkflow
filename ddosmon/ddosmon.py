b947b9852e9cbf72876b0a9607be3e75cc01b8c0
Fyodor Y <fygrave@o0o.nu>
2013-06-13 23:46:36 +0800

    mon script

--- ddosmon/ddosmon.py
+++ ddosmon/ddosmon.py
@@ -8,6 +8,16 @@ import sys
 packs = pyshark.read(sys.argv[1], ['ip.src', 'ip.dst', 'http.host', 'http.request.uri', 'http.user_agent'], 'ip')
 
 packs = list(packs)
+
+gauge = statsd.Gauge('ddos')
+raw = statsd.Raw('ddos')
+useragent = statsd.Gauge('ddos.useragent')
+
+
+packs = list(packs)
 for p in packs:
+        raw.send('pack', 1, p.timestamp)
         print p
+        gauge.send('urllen', len(p["http.request.uri"]))
+        gauge.send('agentlen', len(p["http.user_agent"]))
 
