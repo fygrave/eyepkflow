8536dd3e3cb283f6a1d407219f2dd537ac233861
Fyodor Y <fygrave@o0o.nu>
2013-06-14 10:43:46 +0800

    apparmor hack. TODO: fix later ;)

--- ddosmon/run_d.sh
+++ ddosmon/run_d.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+ln -s /etc/apparmor.d/usr.sbin.tcpdump /etc/apparmor.d/disable/
+apparmor_parser -R /etc/apparmor.d/usr.sbin.tcpdump
+
 tcpdump -i eth1  -s 1500 -n -C 1M -G 5  -W 10   -w /data/dumz_%s ip
