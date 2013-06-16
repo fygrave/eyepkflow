599f713c7051bbaaef3a1cd51dc82b4a05e2bddc
Fyodor Y <fygrave@o0o.nu>
2013-06-16 23:27:03 +0800

    corrections to the shell scrptwq

--- analyzer/runwave.sh
+++ analyzer/runwave.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-./wavelyzer.py w/total.wsp  --wavelet haar --relative 7200 --es localhost:9200 --statsd localhost
+./wavelyzer.py /opt/graphite/storage/whisper/stats/flow/bytes/total.wsp  --wavelet haar --relative 7200 --es localhost:9200 --statsd localhost --relative $[ 60 * 60 * 2 ]
+
